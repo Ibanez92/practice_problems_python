8a51041f1e064635e230fdcdf31e4ce3d16bf7f4
Javier Ibanez <Expertprep2015@gmail.com>
2023-06-05 16:07:03 -0400

    finished first two problems in python

--- wordplay/wordplay.py
+++ wordplay/wordplay.py
@@ -1,2 +1,27 @@
-File_object = open(r"wordplay/sowpods.txt", "r").read().split('\n')
-print(File_object)
+word_file = open(r"wordplay/sowpods.txt", "r").read().split('\n')
+
+# What are all of the words containing UU?
+# for word in word_file:
+#     if "UU" in word:
+#         print(word)
+
+# What are all of the words containing an X and a Y and a Z?
+for word in word_file:
+    if "X" "Y" "Z" in word:
+        print(word)
+
+# What are all of the words containing a Q but not a U?
+
+# What are all of the words that contain the word CAT and are exactly 5 letters long?
+
+# What are all of the words that have no E or A and are at least 15 letters long?
+
+# What are all of the words that have a B and an X and are less than 5 letters long?
+
+# What are all of the words that both start and end with a Y?
+
+# What are all of the words with no vowel and not even a Y?
+
+# What are all of the words that have all 5 vowels, in any order?
+
+# What are all of the words that have all 5 vowels, in alphabetical order?
