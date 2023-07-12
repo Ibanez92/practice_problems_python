00ca152545103120a5d8cd0c777630ccefc7887a
Javier Ibanez <Expertprep2015@gmail.com>
2023-07-12 19:03:47 -0400

    solved 2nd problem in countries section. What countries both begin and end with a vowel?

--- countries/countries.py
+++ countries/countries.py
@@ -4,14 +4,27 @@ country_file = open(r"countries/countries.txt", "r").read().split("\n")
 
 # What are all of the countries that have “United” in the name?
 
-def country_with_united(countries):
-    for country in countries:
-        if "United" in country:
-            print(country)
+# def country_with_united(countries):
+#     for country in countries:
+#         if "United" in country:
+#             print(country)
 
-country_with_united(country_file)
+# country_with_united(country_file)
 
 # What countries both begin and end with a vowel?
+
+
+def countries_begin_end_vowel(countries):
+    countries_with_vowel = []
+
+    for country in countries:
+        if len(country) >= 2 and country[0].lower() in "aeiou" and country[-1].lower() in "aeiou":
+            countries_with_vowel.append(country)
+
+    print(countries_with_vowel)
+
+
+countries_begin_end_vowel(country_file)
 
 # What country names are > 50% vowels?
 
